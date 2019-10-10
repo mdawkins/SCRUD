bc71ed0f290f8fd2e77a90efb02b1844445b5243
Matthew Dawkins <[mattydaw@gmail.com]>
2019-10-10 12:41:09 -0600

    Cleaned up sytle and script links Moved the addedit form to JS the form is created dynamically now

--- indices/index.php
+++ indices/index.php
@@ -19,12 +19,12 @@ if ( isset($_GET["page"]) ) {
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/themes/base/jquery-ui.min.css">
-<!--    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.jqueryui.min.css"> --!>
+<!--<link rel="stylesheet" href="https://cdn.datatables.net/1.10.18/css/dataTables.jqueryui.min.css"> --!>
     <link rel="stylesheet" href="//cdn.datatables.net/colreorder/1.5.1/css/colReorder.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/fixedcolumns/3.2.5/css/fixedColumns.jqueryui.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/fixedheader/3.1.4/css/fixedHeader.jqueryui.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.2/css/responsive.jqueryui.min.css">
-<!--    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/yadcf/0.9.3/jquery.dataTables.yadcf.min.css"> --!>
+<!-- <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.2/css/responsive.jqueryui.min.css"> --!>
+<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/yadcf/0.9.3/jquery.dataTables.yadcf.min.css"> --!>
     <link rel="stylesheet" href="/css/jquery.dataTables.yadcf.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.8/css/select2.min.css">
     
@@ -58,10 +58,11 @@ if ( isset($rowformat) ) {
   </head>
   <body>
 
-<!-- top menu bar --!>
+<!-- start top menu bar --!>
 <?php
 include_once "$approot/templates/topmenubar.php";
 ?>
+<!-- end of top menu bar --!>
 
     <div id="page_container">
       <table class="datatable" id="table_records">
@@ -76,79 +77,7 @@ include_once "$approot/templates/topmenubar.php";
     <div class="lightbox_container">
       <div class="lightbox_close"></div>
       <div class="lightbox_content">
-
-<!-- start of form --!>
-        <h2>##blank##</h2>
-	<form class="form add" id="form_record" data-id="" novalidate>
-<?php
-foreach ( $colslist as $i => $col ) {
-	if ( $col["input_type"] != "noform" && $col["input_type"] != "drilldown" && $col["input_type"] != "crosswalk") {
-		if ( $col["required"] == "yes" ) {
-			$errspan = "<span class='required'>*</span>";
-			$errinput = "required";
-		} else {
-			unset($errspan);
-			unset($errinput);
-		}
-		echo "<div class='input_container'>\n";
-		echo "\t<label for='".$col["column"]."'>".$col["title"].": $errspan</label>\n";
-		echo "\t<div class='field_container'>\n";
-		switch ( $col["input_type"] ) {
-
-		case "text":
-			echo "\t\t<input type=\"text\" class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" value=\"\" $errinput>\n";
-			break;
-
-		case "number":
-		case "currency":
-			echo "\t\t<input type=\"number\" class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" value=\"\" $errinput>\n";
-			break;
-
-		case "date":
-			echo "\t\t<input type=\"date\" class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" value=\"\" $errinput>\n";
-			break;
-
-		case "datetime":
-			echo "\t\t<input type=\"datetime-local\" class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" value=\"\" $errinput>\n";
-			break;
-
-		case "textarea":
-			echo "\t\t<textarea class=\"text textarea\" name=\"".$col["column"]."\" id=\"".$col["column"]."></textarea>\n";
-			break;
-
-		case "checkbox":
-			echo "\t\t<input type=\"checkbox\" class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" value=\"1\" >\n";
-			break;
-
-		case "tableselect":
-			include_once "$funcroot/selecttbllist.php";
-		case "select":
-			if ( $col["multiple"] == "yes" ) { $multiple = "multiple";
-			} else { unset($multiple); }
-			echo "\t\t<select class=\"text\" name=\"".$col["column"]."\" id=\"".$col["column"]."\" ".$size." ".$multiple.">
-			<option value=\"\">Select a ".$col["title"]."</option>\n";
-			$multisels = explode(";", ${$col["column"]});
-			foreach ( $lists[$col["column"]] as $list ) {
-				if ( array_search($list["key"], $multisels) !== false ) { $selected="selected"; }
-				if ( $list["key"] != "selectparent" ) {
-					echo "\t\t\t<option value='".$list["key"]."' $selected >".$list["title"]."</option>\n";
-				} else { $selectnested = "true"; }
-				unset($selected);
-			}
-			echo "\t\t</select>\n";
-		break;
-		}
-		echo "\t</div>\n";
-		echo "</div>\n";
-	}
-}
-?>
-          <div class="button_container">
-            <button type="submit">##blank##</button>
-          </div>
-        </form>
-<!-- end of form --!>
-        
+      <!-- addeditform recordform --!>  
       </div>
     </div>
 
